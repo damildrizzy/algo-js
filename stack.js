c49d2519d138c851c825dc3e38df483a5faf0513
samuel adekoya <samuel.adekoya@centricityinsights.com>
2021-02-18 18:26:20 +0100

    implementing a stack

---
+++ stack.js
@@ -0,0 +1,28 @@
+class Stack {
+    constructor() {
+        this.items = []
+    }
+
+    isEmpty() {
+        return this.items.length === 0
+    }
+
+    push(item) {
+        return this.items.push(item)
+    }
+
+    pop() {
+        return this.items.pop()
+    }
+
+    peek () {
+        return this.items[this.items.length - 1]
+    }
+
+    size() {
+        return this.items.length 
+    }
+}
+
+
+
